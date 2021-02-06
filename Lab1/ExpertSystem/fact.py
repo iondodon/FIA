06c6b72aeeed8af8c08bc6c0b85e38c39ee2a6ca
Ion Dodon <iondodon2@gmail.com>
2021-02-06 21:22:18 +0200

    lab1 logic done

--- Lab1/ExpertSystem/fact.py
+++ Lab1/ExpertSystem/fact.py
@@ -1,37 +1,10 @@
 class Fact:
-    __fact_name = None
-    __parent_fact_name = None
-    __child_facts = []
-    __operand = None
+    fact_name = None
+    parent_fact_name = None
+    child_facts = []
+    operand = None
 
     def __init__(self, fact_name, parent_fact_name, operand):
-        self.__fact_name = fact_name
-        self.__parent_fact_name = parent_fact_name
-        self.__operand = operand
-
-    def set_operand(self, operand):
-        self.__operand = operand
-
-    def get_operand(self):
-        return self.__operand
-
-    def add_child_fact(self, child_fact):
-        self.__child_facts.append(child_fact)
-
-    def set_fact_name(self, fact_name):
-        self.__fact_name = fact_name
-
-    def get_fact_name(self):
-        return self.__fact_name
-
-    def set_parent_fact_name(self, parent_fact_name):
-        self.__parent_fact_name = parent_fact_name
-
-    def get_parent_fact_name(self):
-        return self.__parent_fact_name
-
-    def set_child_facts(self, child_facts):
-        self.__child_facts = child_facts
-
-    def get_child_facts(self):
-        return self.__child_facts
+        self.fact_name = fact_name
+        self.parent_fact_name = parent_fact_name
+        self.operand = operand
--- Lab1/ExpertSystem/main.py
+++ Lab1/ExpertSystem/main.py
@@ -1,23 +1,67 @@
-import csv
-import pprint
 from fact import Fact
-
-pp = pprint.PrettyPrinter(indent=4)
+import csv
 
 knowledge = {}
+
+
+def get_children_list(new_fact) -> list:
+    children_list = []
+    for fact_name in knowledge:
+        fact: Fact = knowledge[fact_name]
+        if fact.parent_fact_name == new_fact.fact_name:
+            children_list.append(fact.fact_name)
+
+    return children_list
+
+
+def set_fact(new_fact):
+    if not (new_fact.operand is None):
+        children_list = get_children_list(new_fact)
+        new_fact.child_facts = children_list
+
+    knowledge[new_fact.fact_name] = new_fact
 
 
 def read_knowledge():
     with open('data.csv', newline='') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         for raw in reader:
-            fact_name = raw[0]
-            parent_fact_name = raw[1]
-            operand = raw[2]
+            fact_name = raw[0] if raw[0] != '' else None
+            parent_fact_name = raw[1] if raw[1] != '' else None
+            operand = raw[2] if raw[2] != '' else None
             new_fact = Fact(fact_name, parent_fact_name, operand)
-            # set_fact(new_fact)
+            set_fact(new_fact)
+
+
+def has_child(fact_name) -> bool:
+    for fact_key in knowledge:
+        fact = knowledge[fact_key]
+        if fact.parent_fact_name == fact_name:
+            return True
+    return False
+
+
+def ask(fact_name) -> bool:
+    response = input(fact_name + "? (y/n): ")
+    return True if response == 'y' else False
+
+
+def check_fact(fact_name) -> bool:
+    fact_state = True
+    fact = knowledge[fact_name]
+    if fact.operand == "and":
+        for child_fact_name in fact.child_facts:
+            fact_state = fact_state and check_fact(child_fact_name)
+        return fact_state
+    elif fact.operand == "or":
+        for child_fact_name in fact.child_facts:
+            if check_fact(child_fact_name) is True:
+                return True
+        return False
+    else:
+        return fact_state and ask(fact_name)
 
 
 if __name__ == '__main__':
     read_knowledge()
-    pp.pprint(knowledge)
+    print(check_fact("tourist"))
